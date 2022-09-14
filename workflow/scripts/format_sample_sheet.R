b4c8fdadc176a49fef5a2461254bcf7fcfc1e579
Bhavya Singh <singhb39@mcmaster.ca>
2022-09-14 10:21:19 -0400

    added scripts, new metadata file, etc

---
+++ workflow/scripts/format_sample_sheet.R
@@ -0,0 +1,39 @@
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+#######################  Format Burkitt Lymphoma Metadata ######################
+
+# The original sample sheet (located in "config/samples.tsv") was genereated 
+# using the gdcquery.py script, which can be found in "workflow/scripts". The
+# information was downloaded through the GDC website, using an API. Additional
+# metadata was obtained from https://ocg.cancer.gov/programs/cgci/data-matrix
+
+################################################################################
+################################################################################
+#################################### SETUP #####################################
+
+library(tidyr)
+
+################################### METADATA ###################################
+# Read in sample information
+samples <- read.csv("config/samples.tsv", sep = "\t")
+# Read in clinical metadata
+metadata <- read.csv("config/metadata.patients.tsv", sep = "\t")
+
+colnames(samples) <- c("case_uuid", "project_id", "aliquot_id", "submitter_id",
+                       "sample_uuid", "sample_type", "sample_type_id",
+                       "sample", "tissue_type", "tumor_descriptor", "case",
+                       "data_category", "data_type", "experimental_strategy",
+                       "file_id", "file_name", "id", "md5sum")
+
+samples_metadata <- merge(samples, metadata, by.x = "case",
+                          by.y = "patient_barcode", all.x = TRUE)
+
+################################### SAVE FILE ###################################
+
+write.table(samples_metadata, 
+          "config/samples_metadata.tsv", 
+          row.names = FALSE, 
+          quote = FALSE,
+          sep = "\t")
